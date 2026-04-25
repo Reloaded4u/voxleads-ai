eb1af1727d099fa56291f991b14220ec71ba1d4c
Reloaded4u <sudeshbhavsar2011@gmail.com>
2026-04-25 15:15:58 +0530

    Add Vobiz WebSocket streaming prototype

--- server.ts
+++ server.ts
@@ -1744,7 +1744,7 @@ async function startServer() {
 });
 }
 
-app.listen(PORT, "0.0.0.0", () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on http://0.0.0.0:${PORT}`);
   startCallQueueWorker();
 });
