b7933273b1d8e9095c7ce112c9f3ca0b6d9d8e28
Reloaded4u <sudeshbhavsar2011@gmail.com>
2026-04-25 15:36:40 +0530

    Fix server listen placement for websocket server

--- server.ts
+++ server.ts
@@ -1742,7 +1742,6 @@ async function startServer() {
    app.get("*", (req, res) => {
   res.sendFile(path.join(distPath, "index.html"));
 });
-}
 
 server.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on http://0.0.0.0:${PORT}`);
